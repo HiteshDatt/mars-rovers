783691c903ba75fc2355d75abdd42f48ce1a2502
Hitesh <hdk.hitesh26@gmail.com>
2021-02-05 19:45:06 +0530

    parallax bg

--- src/components/GalleryComp/GalleryHeader.js
+++ src/components/GalleryComp/GalleryHeader.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function GalleryHeader(){
+    const [Translate, setTranslate] = useState(0);
+
+    function setscroll(){
+        let rate = window.scrollY * 0.3;
+        setTranslate(rate); 
+    }
+
+    window.addEventListener("scroll", setscroll);
+
     return(
         <div className = "gallery-head">
-            <h2>Mars Rover Images</h2>
+            <h2 style= {{transform: `translateY(${Translate}px)`}}>Mars Rover Images</h2>
         </div>
     );
 }
